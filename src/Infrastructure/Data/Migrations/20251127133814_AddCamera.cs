d55cc4762ea25ed120d4ae234e633a11836914d1
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2025-12-01 18:03:27 +0200

    feat: Add camera migration and related files
    - Created migration files for adding Camera and CameraName entities.
    - Updated the Cameras table to include a Name column with a maximum length of 100 characters.
    - Added .m3u8 file for streaming configuration and associated .ts files.
    - Included RTSP transport command for streaming setup.
    - Updated .gitignore to exclude guidelines.md and sqlserver.
    - Added .DotSettings file for project configuration.

---
+++ src/Infrastructure/Data/Migrations/20251127133814_AddCamera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCamera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cameras",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SiteId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Created = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cameras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cameras_Sites_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Sites",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cameras_SiteId",
+                table: "Cameras",
+                column: "SiteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cameras");
+        }
+    }
+}
---
+++ src/Infrastructure/Data/Migrations/20251127140503_AddCameraName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCameraName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Cameras",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Cameras");
+        }
+    }
+}
