c5ea0e021bb692f6157339469856a03ada5fb081
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2025-12-08 17:40:56 +0200

    feat: Enable VLC player in CameraStream component and update RTSP source
    - Replaced placeholder video with an RTSP video stream.
    - Uncommented VLCPlayer component for live streaming.
    - Made minor style tweaks for consistency in the CameraStream component.

--- src/ClientApp/features/cameras/components/CameraStream/CameraStream.tsx
+++ src/ClientApp/features/cameras/components/CameraStream/CameraStream.tsx
@@ -1,41 +1,54 @@
-import { Text, View } from "react-native";
-// import { VLCPlayer, VlCPlayerView } from "react-native-vlc-media-player";
+import {Text, View} from "react-native";
+import {VLCPlayer, VlCPlayerView} from "react-native-vlc-media-player";
 
 import React from "react";
-import { cameraStreamStyles } from "./CameraStream.styles";
-import { useColorPalette } from "@/hooks/useColorPalette";
+import {cameraStreamStyles} from "./CameraStream.styles";
+import {useColorPalette} from "@/hooks/useColorPalette";
 
 interface CameraStreamProps {
-  label?: string;
+    label?: string;
 }
 
+// const videoSource =
+//     "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 const videoSource =
-  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    "rtsp://admin:L264B20A@192.168.100.23:554/cam/realmonitor?channel=1&subtype=0"
 
 const CameraStream: React.FC<CameraStreamProps> = ({
-  label = "Camera Stream",
-}) => {
-  const colorPalette = useColorPalette();
+                                                       label = "Camera Stream",
+                                                   }) => {
+    const colorPalette = useColorPalette();
 
-  return (
-    <View
-      style={[
-        cameraStreamStyles.streamWrapper,
-        { borderColor: colorPalette.primary },
-      ]}
-    >
-      {/*<VLCPlayer*/}
-      {/*  // style={[styles.video]}*/}
-      {/*  videoAspectRatio="16:9"*/}
-      {/*  source={{ uri: videoSource }}*/}
-      {/*/>*/}
-      <Text
-        style={[cameraStreamStyles.streamLabel, { color: colorPalette.text }]}
-      >
-        {label}
-      </Text>
-    </View>
-  );
+    return (
+        <View
+            style={[
+                cameraStreamStyles.streamWrapper,
+                {borderColor: colorPalette.primary},
+            ]}
+        >
+            <VLCPlayer
+                // style={[styles.video]}
+                videoAspectRatio="16:9"
+                source={{uri: videoSource}}
+            />
+            {/*<VlCPlayerView*/}
+            {/*    autoplay={true}*/}
+            {/*    url="https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4"*/}
+            {/*    ggUrl=""*/}
+            {/*    showGG={true}*/}
+            {/*    showTitle={true}*/}
+            {/*    title="Big Buck Bunny"*/}
+            {/*    showBack={true}*/}
+            {/*    onLeftPress={() => {*/}
+            {/*    }}*/}
+            {/*/>*/}
+            <Text
+                style={[cameraStreamStyles.streamLabel, {color: colorPalette.text}]}
+            >
+                {label}
+            </Text>
+        </View>
+    );
 };
 
 export default CameraStream;
