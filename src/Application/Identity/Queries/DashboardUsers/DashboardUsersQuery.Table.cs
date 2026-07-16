4ae91debf89c67fd522b7edb331af8bd90b6beb6
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2026-07-16 16:59:52 +0300

    feat: refactor DashboardUsers and DashboardPersons queries; add table definitions for filtering and sorting

---
+++ src/Application/Identity/Queries/DashboardUsers/DashboardUsersQuery.Table.cs
@@ -0,0 +1,99 @@
+using Application.SeedWork.Queries;
+using Domain.Entities;
+
+namespace Application.Identity.Queries.DashboardUsers;
+
+public sealed partial class DashboardUsersQuery
+{
+    public static readonly TableQueryDefinition<ApplicationUser, DashboardUsersQuery> Table =
+        new(
+            Filters:
+            [
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.TextContains(
+                    "id",
+                    query => query.Id,
+                    user => user.Id
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.TextContains(
+                    "username",
+                    query => query.Username,
+                    user => user.UserName ?? string.Empty
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.TextContains(
+                    "email",
+                    query => query.Email,
+                    user => user.Email ?? string.Empty
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.TextContains(
+                    "phoneNumber",
+                    query => query.PhoneNumber,
+                    user => user.PhoneNumber ?? string.Empty
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.BooleanEquals(
+                    "isEmailConfirmed",
+                    query => query.IsEmailConfirmed,
+                    user => user.EmailConfirmed
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.BooleanEquals(
+                    "isPhoneNumberConfirmed",
+                    query => query.IsPhoneNumberConfirmed,
+                    user => user.PhoneNumberConfirmed
+                ),
+                TableFilterDescriptor<ApplicationUser, DashboardUsersQuery>.DateTimeOffsetSearch(
+                    "lastLoginAt",
+                    query => query.LastLoginAt,
+                    user => user.LastLoginAt
+                )
+            ],
+            Sorts: new Dictionary<string, TableSortDescriptor<ApplicationUser, DashboardUsersQuery>>(
+                StringComparer.OrdinalIgnoreCase
+            )
+            {
+                ["numberId"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "numberId",
+                    user => user.NumberId,
+                    user => user.Id
+                ),
+                ["id"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "id",
+                    user => user.Id,
+                    user => user.Id
+                ),
+                ["username"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "username",
+                    user => user.UserName ?? string.Empty,
+                    user => user.Id
+                ),
+                ["email"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "email",
+                    user => user.Email ?? string.Empty,
+                    user => user.Id
+                ),
+                ["phoneNumber"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "phoneNumber",
+                    user => user.PhoneNumber ?? string.Empty,
+                    user => user.Id
+                ),
+                ["isEmailConfirmed"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "isEmailConfirmed",
+                    user => user.EmailConfirmed,
+                    user => user.Id
+                ),
+                ["isPhoneNumberConfirmed"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "isPhoneNumberConfirmed",
+                    user => user.PhoneNumberConfirmed,
+                    user => user.Id
+                ),
+                ["lastLoginAt"] = TableSortDescriptor<ApplicationUser, DashboardUsersQuery>.Create(
+                    "lastLoginAt",
+                    user => user.LastLoginAt ?? DateTimeOffset.MinValue,
+                    user => user.Id
+                )
+            },
+            DefaultSort: query =>
+                query
+                    .OrderBy(user => user.UserName ?? string.Empty)
+                    .ThenBy(user => user.Email ?? string.Empty)
+                    .ThenBy(user => user.Id)
+        );
+}
