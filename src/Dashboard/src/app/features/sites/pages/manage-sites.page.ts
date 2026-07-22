0ee5eee6b14863ac0334b821ced9724871ecd6f7
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2026-07-22 10:55:26 +0300

    feat: add Manage Sites page and integrate into dashboard navigation

---
+++ src/Dashboard/src/app/features/sites/pages/manage-sites.page.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-sites-page',
+  templateUrl: './manage-sites.page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ManageSitesPage {}
