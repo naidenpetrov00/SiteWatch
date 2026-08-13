cfbce6c413ffb27f6985f811ab6dc27091ca7558
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2026-08-13 15:27:07 +0300

    feat: add refresh control to Info component for site data refetching

--- src/ClientApp/app/(app)/Site/[siteId]/(tabs)/Info.tsx
+++ src/ClientApp/app/(app)/Site/[siteId]/(tabs)/Info.tsx
@@ -1,13 +1,15 @@
 import Details from "@/features/sites/info/component/Details/Details/Details";
 import React from "react";
-import { ScrollView } from "react-native";
+import { RefreshControl, ScrollView } from "react-native";
 import Summary from "@/features/sites/info/component/Summary/Summary";
 import { useColorPalette } from "@/hooks/useColorPalette";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useGetSitesByUserId } from "@/features/sites/api/get-sites-by-user";
 
 const Info = () => {
   const colorPalette = useColorPalette();
   const insets = useSafeAreaInsets();
+  const { isRefetching, refetch } = useGetSitesByUserId();
 
   return (
     <ScrollView
@@ -19,6 +21,14 @@ const Info = () => {
         paddingBottom: insets.bottom + 80,
         paddingTop: 16,
       }}
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefetching}
+          onRefresh={refetch}
+          tintColor={colorPalette.primary}
+          colors={[colorPalette.primary]}
+        />
+      }
     >
       <Summary />
       <Details />
